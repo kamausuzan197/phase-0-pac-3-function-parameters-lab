176ffa0a493f8c34ec5f9558a463889d44cae1d5
kamausuzan197 <kamaususan197@gmail.com>
2022-06-02 21:00:56 +0300

    complete task

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-function introduction(name){
-    return`Hi, my name is ${name}.`
+
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);
 }
 
-introduction("Aki");
-
-function introductionWithLanguage(name,language){
-    return`Hi, my name is ${name} and I am learning to program in ${language}.`
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 
-introductionWithLanguage("Aki","Ember.js");
-
-function introductionWithLanguageOptional(name,language){
-    return`Hi, my name is ${name} and I am learning to program in ${language}.`
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-
-introductionWithLanguageOptional("Aki","Javascript");
-function introductionWithLanguageOptional(name,language=[]){
-    language.push(Javascript);
-    return`Hi, my name is ${name} and I am learning to program in ${language}.`
-}
-
-introductionWithLanguageOptional("Gracie","Javascript");
